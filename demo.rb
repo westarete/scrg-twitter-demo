f923003d53d6b338c56d2450a5c8cc108c6929ea
Scott Woods <scott@westarete.com>
2011-10-12 19:12:11 -0400

    Text message Scott about the latest homecoming news

--- demo.rb
+++ demo.rb
@@ -3,4 +3,13 @@ require 'twitter'
 require 'twilio-ruby'
 
 tweet = Twitter::Search.new.from("onwardstate").containing("homecoming").fetch.first
-puts tweet.text
+
+account_sid = 'xxx'
+auth_token = 'xxx'
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+@client.account.sms.messages.create(
+  :from => '+14155992671',
+  :to => '+xxx',
+  :body => "Onward State just said: #{tweet.text}"
+)
