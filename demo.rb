06d08cc0c6a2c910fae338da68d9d297df293c00
Scott Woods <scott@westarete.com>
2011-10-12 19:00:03 -0400

    Fetch latest tweet that is sent to @pennstateruby

--- demo.rb
+++ demo.rb
@@ -3,4 +3,4 @@ require 'twitter'
 require 'pp'
 
 search = Twitter::Search.new
-puts search.hashtag("ruby").language("ja").no_retweets.per_page(1).fetch.first.text
+text = search.containing("@pennstateruby").fetch.first.text
