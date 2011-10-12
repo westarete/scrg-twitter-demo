6f337a5f3a150e386d486e44c163f7bdaa691c04
Scott Woods <scott@westarete.com>
2011-10-12 19:05:04 -0400

    Post a tweet

--- demo.rb
+++ demo.rb
@@ -2,6 +2,11 @@ require 'rubygems'
 require 'twitter'
 require 'pp'
 
-search = Twitter::Search.new
-text = search.containing("@pennstateruby").fetch.first.text
-system "say #{text}" 
+Twitter.configure do |config|
+  config.consumer_key = 'xxx'
+  config.consumer_secret = 'xxx'
+  config.oauth_token = 'xxx'
+  config.oauth_token_secret = 'xxx'
+end
+
+Twitter.update("Live demo at the @RealInnoBlue garage for ruby group!")
